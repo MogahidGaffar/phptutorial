0c9ae249659faa07aa746cbe3cce7907041df9a4
Mogahid <mogahidgaffar@gmail.com>
2023-11-16 15:09:27 +0300

    php strings

---
+++ Basic/phpstring.php
@@ -0,0 +1,33 @@
+<?php 
+
+
+$string_1 = "This is a string in double quotes";
+$string_2 = "This is a somewhat longer, singly quoted string";
+$string_39 = "This string has thirty-nine characters";
+$string_0 = ""; // a string with zero characters
+
+
+
+
+$variable = "name";
+   $literally = 'My $variable will not print!\\n';
+   
+   print($literally);
+   print "<br />";
+   
+   $literally = "My $variable will print!\\n";
+   
+   print($literally);
+
+   $string1="Hello World";
+   $string2="1234";
+   
+   echo $string1 . " " . $string2; // Merge two strings
+
+   echo strlen("Hello world!"); //The strlen() function is used to find the length of a string.
+
+   echo strpos("Hello world!","world"); //The strpos() function is used to search for a string or character within a string.
+
+
+
+?>
