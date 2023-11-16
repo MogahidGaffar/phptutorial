9ba68ce98f4682107ed133513cdcd84452d9fa06
Mogahid <mogahidgaffar@gmail.com>
2023-11-16 15:01:42 +0300

    example of PHP echo and print Statements

---
+++ Basic/phpecho.php
@@ -0,0 +1,13 @@
+<?
+
+//PHP echo and print Statements
+
+
+echo "<h2>i Love PHP </h2>";
+echo "Hello world!<br>";
+echo "I'm learning PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+
+
+
+?>
