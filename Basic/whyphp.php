4dc79936ee07285ffc544bb333c6b766bbd9d1b0
Mogahid <mogahidgaffar@gmail.com>
2023-11-16 14:53:41 +0300

    whyphp.php

---
+++ Basic/whyphp.php
@@ -0,0 +1,9 @@
+<?php
+echo "Why Php? </br>";
+$txt = "PHP";
+echo "$txt  runs on various platforms</br>";
+echo "$txt   is compatible with almost all servers used today (Apache, IIS, etc.)</br>";
+echo "$txt  supports a wide range of databases</br>";
+echo "$txt   is free. Download it from the official PHP resource</br>";
+echo "$txt  is easy to learn and runs efficiently on the server side</br>";
+?>
